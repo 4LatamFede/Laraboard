6f4d5563739593e4cb50ccd558991b1a294737f4
4LatamFede <federico.baez@4latam.com>
2018-05-24 12:44:34 -0300

    1.0

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,68 @@
+<?php
+
+return [
+
+    /**
+     *
+     * Shared translations.
+     *
+     */
+    'title' => 'Laravel Interactive',
+    'next' => 'Siguente',
+
+
+    /**
+     *
+     * Home page translations.
+     *
+     */
+    'welcome' => [
+        'title'   => 'Bienvenido a Laravel Interactiva',
+        'message' => 'Bienvenido a la configuración.',
+    ],
+
+
+    /**
+     *
+     * Requirements page translations.
+     *
+     */
+    'requirements' => [
+        'title' => 'Requerimientos',
+    ],
+
+
+    /**
+     *
+     * Permissions page translations.
+     *
+     */
+    'permissions' => [
+        'title' => 'Permisos',
+    ],
+
+
+    /**
+     *
+     * Environment page translations.
+     *
+     */
+    'environment' => [
+        'title' => 'Configuración del entorno',
+        'save' => 'Guardar .env',
+        'success' => 'Su configuración de archivo .env se ha guardado.',
+        'errors' => 'No se puede guardar el archivo .env, créelo manualmente.',
+    ],
+
+
+    /**
+     *
+     * Final page translations.
+     *
+     */
+    'final' => [
+        'title' => 'Finalizado',
+        'finished' => 'La aplicación ha sido instalada con éxito.',
+        'exit' => 'Haga clic aquí para salir',
+    ],
+];
